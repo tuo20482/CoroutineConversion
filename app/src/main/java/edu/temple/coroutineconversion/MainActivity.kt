be934b877bffde1e23898b2a4ec76c25bfe9bcae
Addison Migash <tuo20482@temple.edu>
2023-11-02 14:34:24 -0400

    Replaced threads with coroutines

--- app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
+++ app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
@@ -2,21 +2,16 @@ package edu.temple.coroutineconversion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.widget.Button
 import android.widget.ImageView
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
 
     //TODO (Refactor to replace Thread code with coroutines)
 
     private lateinit var cakeImageView: ImageView
-
-    private val handler = Handler(Looper.getMainLooper()) {
-        cakeImageView.alpha = it.what / 100f
-        true
-    }
+    private val scope = CoroutineScope(Dispatchers.Main)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,12 +20,16 @@ class MainActivity : AppCompatActivity() {
         cakeImageView = findViewById(R.id.imageView)
 
         findViewById<Button>(R.id.revealButton).setOnClickListener{
-            Thread{
-                repeat(100) {
-                    handler.sendEmptyMessage(it)
-                    Thread.sleep(40)
+            scope.launch {
+                for (i in 0..100) {
+                    cakeImageView.alpha = i / 100f
+                    delay(40)
                 }
-            }.start()
+            }
         }
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        scope.cancel()
     }
 }
